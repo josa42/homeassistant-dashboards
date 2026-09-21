8e8eb059b6436246aa4f415dfc1196722ffc7106
Josa Gesell <josa@gesell.me>
2026-09-21 23:48:34 +0200

    chore: bump version to 0.2.2

--- josa-dashboards.js
+++ josa-dashboards.js
@@ -1,4 +1,4 @@
-const STRATEGY_VERSION = "0.2.1";
+const STRATEGY_VERSION = "0.2.2";
 
 // Which build is loaded is the first question on any bug report, and a
 // dashboard resource has nowhere else to say so. scripts/release.sh keeps this
